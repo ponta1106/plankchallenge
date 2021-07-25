2c74f646db928aee649a158d8c2f378bd4186f0e
Nobuya Tabata <qqkz2qf9k@kind.ocn.ne.jp>
2021-07-25 19:24:29 +0900

    結果表示モーダルの作成

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
-const reset = document.getElementById('reset');
 const timer = document.getElementById('timer');
+const commentArea = document.getElementById('commentArea');
+const preCommentArea = document.getElementById('preCommentArea');
+const resultImage = document.getElementById('resultImage');
+const modal = document.querySelector('.modal');
 
+const comments = [
+  '「クリスティアーノ・ロナウドレベル」です',
+  '「鉄板レベル」です',
+  '「豆腐レベル」です',
+  '「わたあめレベル」です',
+];
+
+const resultImages = [
+  'ronaldo.jpg',
+  'teppan.jpg',
+  'tofu.jpg',
+  'wataame.jpg',
+];
+
+function judgeTime(result){
+  if(result >= "01:00:000"){
+    comment = comments[0];
+    resultImage.src = resultImages[0];
+  }else if(result >= "00:40:000"){
+    comment = comments[1];
+    resultImage.src = resultImages[1];
+  }else if(result >= "00:20:000"){
+    comment = comments[2];
+    resultImage.src = resultImages[2];
+  }else{
+    comment = comments[3];
+    resultImage.src = resultImages[3];
+  };
+};
+
+const difference = 0;
 let startTime;
 let elapsedTime = 0;
 let timerId;
@@ -31,16 +65,15 @@ function countUp(){
 start.addEventListener('click', function(){
   startTime = Date.now();
   countUp();
+  start.style.display = 'none';
+  stop.style.display = 'block';
 });
 
 stop.addEventListener('click', function(){
   clearTimeout(timerId);
   timeToAdd += Date.now() - startTime;
+  judgeTime(timer.textContent);
+  modal.style.left = '0px'
+  preCommentArea.textContent = 'あなたの腹筋力は...';
+  commentArea.textContent = comment;
 })
-
-reset.addEventListener('click', ()=>{
-  elapsedTime = 0;
-  timeToAdd = 0;
-  updateTimeText();
-})
-
